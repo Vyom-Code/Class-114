64b40a6c5ae19f2e1d71bea3055aa952aeefc485
Vyom-Code <82333325+Vyom-Code@users.noreply.github.com>
2022-02-19 10:56:21 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,26 @@ function preload(){
 function setup(){
     canvas = createCanvas(600,600)
     canvas.position(950,300);
+    video = createCapture(VIDEO)
+    video.size(600,600);
+    video.hide();
+
+    PoseNet= ml5.poseNet(video, modelLoaded);
+    PoseNet.on('pose', gotPoses);
 }
-
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results){
+    if(results.length> 0){
+        console.log("nose x= "+results[0].pose.nose.x);
+        console.log("nose y= "+results[0].pose.nose.y);
+    }
+}
 function draw(){
-
+image(video, 0, 0, 600, 600);
 }
 
 function take_snapshot(){
     save('Vyom_01.png');
-}``
+}
